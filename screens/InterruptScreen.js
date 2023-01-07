09013767414ff1413f16abcdcca4c9a3eb7e6518
catri <catrini.alessio2@gmail.com>
2023-01-07 17:09:45 +0100

    Non lo so più, aiuto, vi prego

--- screens/InterruptScreen.js
+++ screens/InterruptScreen.js
@@ -1,11 +1,12 @@
-
-
-import { Button, View,Text } from "react-native";
+import { Button, View, Text, TouchableOpacity} from "react-native";
 
 export default function InterruptScreen({ navigation }) {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white">Ciao, sono Tia</Text>
+      <Text className="text-white">
+        Una delle soluzioni per evitare che si verifichino <TouchableOpacity onPress={()=> {navigation.navigate("Deadlock")}}><Text><Text className="text-white">deadlock</Text></Text></TouchableOpacity>, starvation
+        o altri problemi possiamo utilizzare
+      </Text>
     </View>
   );
 }
--- screens/SharedSpacesScreen.js
+++ screens/SharedSpacesScreen.js
@@ -1,4 +1,5 @@
-import { Button, View, Text, Dimensions } from "react-native";
+import { Children } from "react";
+import { Button, View, Text, Dimensions, Animated } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Svg, Circle, Line, Image } from "react-native-svg";
 
@@ -9,14 +10,40 @@ export default function SharedSpacesScreen({ navigation }) {
   const FatherRadius = 100;
   return (
     <Svg className="bg-black">
-      <Image
-        style={{ borderRadius: FatherRadius }}
-        x={screenWidth / 2 - FatherRadius}
-        y={126 - FatherRadius}
-        width={FatherRadius * 2}
-        height={FatherRadius * 2}
-        href={require("../images/Ada_Lovelace.jpg")}
-      />
+      <View
+        style={{
+          position: "absolute",
+          top: 50,
+          left: 0,
+          right: 0,
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            width: 365,
+            height: 175,
+            borderRadius: 125,
+            borderWidth: 7.5,
+            backgroundColor: "green",
+            borderColor: "white",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              textAlign: "center",
+              fontSize: 30,
+              fontWeight: "bold",
+            }}
+          >
+            Come gestire spazi di memoria condivisi senza evitare deadlock
+          </Text>
+        </View>
+      </View>
+
       <Image
         style={{ borderRadius: childRadius }}
         x={childRadius - childRadius}
@@ -28,50 +55,43 @@ export default function SharedSpacesScreen({ navigation }) {
           navigation.navigate("Semafori");
         }}
       />
-      
+
       <Image
         style={{ borderRadius: childRadius }}
-        x={screenWidth / 2 - childRadius * 2}
+        x={screenWidth / 2 - childRadius * 2 - 50}
         y={600}
-        width={childRadius * 2}
-        height={childRadius * 2}
+        width={childRadius * 3}
+        height={childRadius * 2.5}
         href={require("../images/CartelloStop.jpg")}
         onPress={() => {
-            navigation.navigate("SpiegazioneInterrupt")
+          navigation.navigate("SpiegazioneInterrupt");
         }}
       />
-      
       <Image
         style={{ borderRadius: childRadius }}
-        x={screenWidth / 2 +20}
+        x={screenWidth / 2 + 20}
         y={550}
         width={childRadius * 2}
         height={childRadius * 2}
         href={require("../images/PetersonProvvisorio.png")}
         onPress={() => {
-            navigation.navigate("AlgoritmoPeterson")
+          navigation.navigate("AlgoritmoPeterson");
         }}
-      />
-      <Circle
-        cx={screenWidth - childRadius}
-        cy={400}
-        r={childRadius}
-        fill="transparent"
       />
       <Image
         style={{ borderRadius: childRadius }}
-        x={screenWidth -childRadius*2}
+        x={screenWidth - childRadius * 2}
         y={371}
         width={childRadius * 2}
         height={childRadius * 2}
         href={require("../images/TestAndSetProvvisorio.png")}
         onPress={() => {
-            navigation.navigate("TestAndSet")
+          navigation.navigate("TestAndSet");
         }}
       />
       <Line
         x1={screenWidth / 2}
-        y1={126}
+        y1={126 + FatherRadius}
         x2={childRadius}
         y2={400 - 38}
         stroke="white"
@@ -79,15 +99,15 @@ export default function SharedSpacesScreen({ navigation }) {
       />
       <Line
         x1={screenWidth / 2}
-        y1={126}
-        x2={screenWidth / 2 - childRadius}
+        y1={126 + FatherRadius}
+        x2={screenWidth / 2 - childRadius * 1.5}
         y2={600}
         stroke="white"
         strokeWidth="2"
       />
       <Line
         x1={screenWidth / 2}
-        y1={126}
+        y1={126 + FatherRadius}
         x2={screenWidth / 2 + childRadius}
         y2={600}
         stroke="white"
@@ -95,21 +115,12 @@ export default function SharedSpacesScreen({ navigation }) {
       />
       <Line
         x1={screenWidth / 2}
-        y1={126}
+        y1={126 + FatherRadius}
         x2={screenWidth - childRadius}
         y2={400}
         stroke="white"
         strokeWidth="2"
       />
-      <Text
-        x={screenWidth / 2}
-        y={126}
-        fontSize={20}
-        textAnchor="middle"
-        fill="black"
-      >
-        Parent
-      </Text>
     </Svg>
   );
 }
